ddaacc1820bd943b1169ce393e5d8695249e9793
Hyerim JI <rimirimijj@gmail.com>
2025-12-03 06:28:50 +0900

    상태코드 명시적으로 작성

--- server.js
+++ server.js
@@ -15,14 +15,32 @@ let posts = [
 ];
 //Get all posts
 app.get("/api/posts", (req, res) => {
-  console.log(req.query);
-  res.json(posts);
+  const limit = parseInt(req.query.limit); //postman에 http://localhost:8000/api/posts?limit=2 이런식으로 제한 확인 가능
+
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  // 제한 없을 때 _전체 목록
+  res.status(200).json(posts);
+
+  //   console.log(req.query);
+  //   res.json(posts);
 });
 
 //Get single post
 app.get("/api/posts/:id", (req, res) => {
   const id = parseInt(req.params.id); //문자열 ->숫자로 //parseInt 오타 주의
-  res.json(posts.filter((p) => p.id === id)); //위에 posts 객체의 id가 여기 전달된 id와 같은지 유무 확인
+  const post = posts.find((post) => post.id === id);
+
+  //존재하지 않을 떄
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+  // 존재할때 _단일 게시글
+  res.status(200).json(post);
+  //   res.status(200).json(posts.filter((p) => p.id === id)); //위에 posts 객체의 id가 여기 전달된 id와 같은지 유무 확인
   //   console.log(req.params.id);
   //   res.json(posts);
 });

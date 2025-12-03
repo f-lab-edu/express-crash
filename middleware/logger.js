371744f88ef93f46e015d28a97619019ff320a3c
Hyerim JI <rimirimijj@gmail.com>
2025-12-04 02:27:11 +0900

    middleware Logger만들고 colors 패키지로 로그 색 넣기

---
+++ middleware/logger.js
@@ -0,0 +1,20 @@
+import color from "colors";
+
+const methodColors = {
+  GET: "green",
+  POST: "blue",
+  PUT: "yellow",
+  DELETE: "red",
+};
+//미들웨어 Logger만들기
+const logger = (req, res, next) => {
+  //인증,로그,권한 체크는 req
+  const color = methodColors[req.method] || "white";
+  // next는 마지막에 호출 후 다음 미들웨어/라우트로 흐름 보냄
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+
+export default logger;
--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,9 @@ let posts = [
   { id: 2, title: "Post Two" },
   { id: 3, title: "Post Three" },
 ];
+
 //Get all posts
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   //경로 중복 않기 위해 /로 변경
   const limit = parseInt(req.query.limit); //postman에 http://localhost:8000/api/posts?limit=2 이런식으로 제한 확인 가능
 
@@ -23,16 +24,17 @@ router.get("/", (req, res) => {
 });
 
 //Get single post
-router.get(":id", (req, res) => {
+router.get("/:id", (req, res, next) => {
+  ///:id에 슬래시가 있어야 라우팅 동작함
   //경로 중복 않기 위해 :id로 변경
   const id = parseInt(req.params.id); //문자열 ->숫자로 //parseInt 오타 주의
-  const post = posts.find((post) => post.id === id);
+  const post = posts.find((p) => p.id === id);
 
   //존재하지 않을 떄
   if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `A post with the id of ${id} was not found` });
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
   // 존재할때 _단일 게시글
   res.status(200).json(post);
@@ -41,4 +43,50 @@ router.get(":id", (req, res) => {
   //   res.json(posts);
 });
 
+//Create new post
+router.post("/", (req, res, next) => {
+  // console.log(req.body); // { title: '...'} 형태
+  console.log("BODY: ", req.body); //뭐지 이거 추가하니까 갑자기 POST에 Brads입력 됨
+
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    const error = new Error(`Please indclude a title`);
+    error.status = 400;
+    return next(error);
+  }
+  posts.push(newPost); //post로 오타났었음. 배열추가한단
+  res.status(201).json(posts); //게시물 넣고 전달 계속
+});
+
+//Update Post 업데이트기능
+router.put("/:id", (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id); //각 게시글 id 유효성 확인
+  if (!post) {
+    //id가 아니면
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
+  }
+  post.title = req.body.title; //제목을 요청해서 가져옴
+  res.status(200).json(post);
+});
+
+//Delete Post 삭제!
+router.delete("/:id", (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id); //각 게시글 id 유효성 확인
+  if (!post) {
+    //id가 아니면
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
+  }
+  posts = posts.filter((post) => post.id !== id); //post id가 존재하는데, filter 로 해당 id를 제외한 새 배열을 만들고 posts에 다시 할당
+  res.status(200).json(post); //삭제 후 남은 posts 객체배열 변수를 응답
+});
 export default router;
